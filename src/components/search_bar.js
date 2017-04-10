4da3cc3cbacac36a2d61061d4705397b1c95cce9
phyrionX <rgcmb@hotmail.com>
2017-04-10 16:11:55 +0200

    React youtube

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,12 +9,17 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.tern}
-          onChange={event => this.setState({tern: event.target.value})}/>
+          onChange={event => this.onInputChange(event.target.value)}/>
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
